9b1de93caf1b3f0e90d65c38c7c2390af440f509
sara <sarahkungfu32@gmail.com>
2026-04-09 19:32:37 +0100

    exs recursividade

---
+++ AlunoRecursividade.cs
@@ -0,0 +1,18 @@
+namespace ExerciciosAula02B;
+  public class AlunoRecursividade
+  {
+    public void Executar()
+
+    {
+      
+      Console.WriteLine("==================================\n");
+      Console.WriteLine("\n\n 1. EXERCÍCIOS DE RECURSIVIDADE");
+      Console.WriteLine("==================================\n");
+
+      Console.WriteLine("==================================\n");
+      Console.WriteLine("\n\n  EXERCÍCIO 5: Factorial Simulado (Recursivo)");
+      Console.WriteLine("==================================\n");
+
+    }
+
+  }
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ExerciciosAula02B;
+
+
+ class Program
+{
+    
+static void Main(string [] args)
+    {
+      Console.WriteLine();  
+    }
+}
