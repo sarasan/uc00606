a42da39bdb88fb33a1108ac19f05677fc90b4dc8
sara <sarahkungfu32@gmail.com>
2026-04-09 19:37:34 +0100

    updt

--- AlunoRecursividade.cs
+++ AlunoRecursividade.cs
@@ -16,3 +16,13 @@ namespace ExerciciosAula02B;
     }
 
   }
+
+  /*def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+# Example usage
+result = factorial(5)
+print(result)  # Output: 120 */
